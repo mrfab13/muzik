9164c101c70c424b763f34f0a758037cde65f17d
Henry <henry983615@gmail.com>
2023-05-22 23:49:20 +1200

    Migrate to maria

--- app.js
+++ app.js
@@ -1,101 +1,79 @@
+const config = require("./config.json");
+
+const mariadb = require('mariadb');
 const express = require('express')
-const mongoose = require('mongoose')
-const passport = require("passport")
-const bodyParser = require("body-parser")
-const LocalStrategy = require("passport-local")
-const passportLocalMongoose = require("passport-local-mongoose")
-const flash = require('connect-flash');
+
+const pool = mariadb.createPool(config.dbInfo);
+
+var dbConnection;
+
+// Connect to db
+async function InitaliseDBConnection() {
+  try 
+  {
+    dbConnection = await pool.getConnection();
+    const res1 = await dbConnection.query("USE USERDATA");
+    AddNewUser("Henry", "Password");
+    const res = await dbConnection.query("SELECT * FROM USERS");
+	  console.log(res);
+  } 
+  catch (err) 
+  {
+    console.log(err);
+  }
+  return;
+}
+
+async function AddNewUser(Username, Password){
+  const res = await dbConnection.query("INSERT INTO USERS (USERNAME, PASSWORD) VALUES ('" + Username + "', '" + Password + "')");
+	console.log(res);
+}
+
+InitaliseDBConnection();
 
 const app = express()
-const port = 3000
-
-mongoose.connect('mongodb://127.0.0.1/data', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('Database connected!');
-})
-.catch(err => {
-   console.error('Error connecting to database:', err);
-});
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true }
-});
-
-userSchema.plugin(passportLocalMongoose);
-
-const User = mongoose.model('User', userSchema);
-
-app.use(flash());
-
-app.use(bodyParser.json());
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(require("express-session")({
-    secret: "Rusty is a dog",
-    resave: false,
-    saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.use(express.static(__dirname + '/public'));
+const port = 80
 
 app.get('/', (req, res) => {
-  res.render('login.ejs');
+   res.render('login.ejs');
 })
 
-app.post('/', passport.authenticate('local', { 
-  successRedirect: '/muzik', 
-  failureRedirect: '/', 
-  failureFlash: true 
-}));
+// app.post('/', passport.authenticate('local', { 
+//   successRedirect: '/muzik', 
+//   failureRedirect: '/', 
+//   failureFlash: true 
+// }));
 
 app.get('/register', (req, res) => {
-  res.render('register.ejs');
+   res.render('register.ejs');
 });
 
-app.post('/register', async (req, res) => {
-  try {
-    const user = new User({ username: req.body.username, password: req.body.password });
-    await User.register(user, req.body.password);
-    res.redirect('/');
-  } catch (error) {
-    res.render('error.ejs', { error });
-  }
-});
+// app.post('/register', async (req, res) => {
+//   try {
+//     const user = new User({ username: req.body.username, password: req.body.password });
+//     await User.register(user, req.body.password);
+//     res.redirect('/');
+//   } catch (error) {
+//     res.render('error.ejs', { error });
+//   }
+// });
 
-app.get('/muzik', isLoggedIn, (req, res) => {
-  res.render('music.ejs');
-});
+// app.get('/muzik', isLoggedIn, (req, res) => {
+//    res.render('music.ejs');
+// });
 
-app.get('/henryfat', (req, res) => {
-  res.send('ha gottem');
-});
+// app.get('/logout', (req, res) => {
+//   req.logout();
+//   res.redirect('/');
+// });
 
-app.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
-});
-
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    res.redirect('/');
-  }
-}
+// function isLoggedIn(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     return next();
+//   } else {
+//     res.redirect('/');
+//   }
+// }
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
